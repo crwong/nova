0f25f941caf28408bcffb7866e34e273ccb1fbec
David Zeng <david.zeng@rubrik.com>
2016-03-30 21:51:37 -0700

    Basic models added

--- app/models.py
+++ app/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=256)
+
+class Interviewer(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    level = models.CharField(max_length=256)
+    skills = models.ManyToManyField(Skill)
+
+class Interview(models.Model):
+    time = models.DateTimeField()
+    interviewer = models.ForeignKey(Interviewer, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    candidate = models.CharField(max_length=256)
